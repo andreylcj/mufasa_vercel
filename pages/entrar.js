0ce8fb9bdee8ddc01f577247ac3b0cf80b9d150b
Andrey <andreylcj@gmail.com>
2021-01-21 18:28:19 -0300

    update to see env variable

--- pages/entrar.js
+++ pages/entrar.js
@@ -10,6 +10,8 @@ import { useRouter } from 'next/router'
 
 function Login() {
 
+    console.log('teste variable => ' + process.env.ACCESS_TOKEN_SECRET)
+
     const [state, dispatch] = useContext(DataContext);
     const { loading } = state
     const { userData } = state
