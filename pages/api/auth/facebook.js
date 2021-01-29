f8c104c2186012448954915c2c7e09b1e76dda64
gumagu01 <gustavomaringoulart@gmail.com>
2021-01-29 12:36:05 -0300

    eeu tudo errado

--- pages/api/auth/facebook.js
+++ pages/api/auth/facebook.js
@@ -1,35 +1,50 @@
-import React, { Component } from 'react';
-import FacebookLogin from 'react-facebook-login'
+import React, { Component, useState, useContext, useEffect } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
 import ReactDOM from 'react-dom';
+import Cookie from 'js-cookie';
+import { useRouter } from 'next/router';
+import {postData} from '../../../assets/utils/fetchData'
+import { DataContext } from '../../../store/GlobalState';
+import { ACTION } from '../../../store/Actions';
 
 
-export default class Facebook
- extends Component {
-    
-   
 
-    state = {
-         isLoggedin: false,
-         userID: '',
-         name:'',
-         email:'',
-         picture:','
-     }
+export default function Facebook()
+  {
+    const [state, dispatch] = useContext(DataContext);
 
-    responseFacebook = response =>{
+    const { userData } = state;
+    const { auth } = state;
+    const [submitStatus, setSubmitStatus] = useState({
+        emailMessage: '',
+        passwordMessage: '',
+        emailNotExist: '',
+      });
+
+    const {email}= useState('');
+    const {isLoggedin} = useState(false);
+    const {userID}= useState('');
+    const {name}=useState('');
+    const {perfil}=useState('');
+    const {setState} = useState();
+
+    const responseFacebook = response =>{
          //console.log(response);
-         this.setState({
-             isLoggedin: true,
-             userID: response.userID,
-             name: response.name,
-             email: response.email,
-             picture: response.picture.data.url
-         })
+         setState(
+             isLoggedin= true,
+             userID= response.userID,
+             name= response.name,
+             email= response.email,
+             picture= response.picture.data.url
+         )
+
+        
      }
 
     componentClicked = () => console.log("clicked");
 
-    render() {
+   
         let fbContent;
         if(this.state.isLoggedin){
             fbContent = (
@@ -60,4 +75,5 @@ export default class Facebook
             </div>
         )
     }
-}
+    
+
