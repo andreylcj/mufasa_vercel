69f2365ed16e3c1a38222e926f86efb1e6cb96d5
Andrey <andreylcj@gmail.com>
2021-03-11 23:04:28 -0300

    update

--- sections/OldUserCeiInfo/index.js
+++ sections/OldUserCeiInfo/index.js
@@ -41,7 +41,7 @@ function OldUserCeiInfo() {
         <FormContainer.Bg style={{ height: 'initial' }}>
           <FormContainer.LeftSide>
             <SecContainer.Title style={{ flexDirection: 'row', justifyContent: 'start', color: '#8E8E8E' }}>
-              Olá, [user email or user first name if exist]!
+              Olá, [Usuário]!
             </SecContainer.Title>
             <img src="/images/oldUser/Lion_Sun.webp" alt="Leão filhote" />
             <p>
